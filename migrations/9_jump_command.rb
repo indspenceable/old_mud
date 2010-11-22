3948f972f0f57a403f45dc6f1c8dca800ed6abc9
Danny Spencer <dspencer@oberlin.edu>
2010-11-22 16:02:44 -0500

    made an example jump command, to show proof of concept of midgame migrations.

---
+++ migrations/9_jump_command.rb
@@ -0,0 +1,13 @@
+module Mud
+  module Commands
+    class Jump < Command
+      def initialize
+        super("jump",[])
+      end
+      def enact player, args
+        player.room.echo "#{player.name} jumps up and down.", [player]
+        player.hear_line "You jump up and down"
+      end
+    end
+  end
+end
