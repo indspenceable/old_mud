c3684ca4d9f5455d42070657a5b2b4f6c2710c6e
Danny Spencer <dspencer@oberlin.edu>
2010-11-22 22:34:21 -0500

    Fixed location of shutdown command.

--- migrations/6_basic_commands.rb
+++ migrations/6_basic_commands.rb
@@ -78,5 +78,16 @@ module Mud
       end
     end
     GlobalCommands << Quit.new
+
+
+    class Shutdown < Command
+      def initialize
+        super("shutdown",[])
+      end
+      def enact player, args
+        EventMachine::stop_event_loop
+      end
+    end
+    GlobalCommands << Shutdown.new
   end
 end
