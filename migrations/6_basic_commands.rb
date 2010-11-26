3fd06010782701c9a8bde92b03f51ffa16cefc8b
Danny Spencer <dspencer@oberlin.edu>
2010-11-25 23:19:47 -0500

    Now uses player displayname. Also, added a number of new commands.

--- migrations/6_basic_commands.rb
+++ migrations/6_basic_commands.rb
@@ -37,7 +37,7 @@ module Mud
         super("say", [])
       end
       def enact player, args
-        player.room.echo "#{player.name} says: \"#{args}\"", [player]
+        player.room.echo "#{player.display_name} says: \"#{args}\"", [player]
         player.hear_line "You say: \"#{args}\""
       end
     end
@@ -51,7 +51,7 @@ module Mud
       def enact player, args
         player.hear_line player.room.name, :yellow
         player.hear_line player.room.description
-        player.hear_line player.room.players.reject{|p| p == player}.map{|p|p.name}.join(", "), :blue
+        player.hear_line player.room.players.reject{|p| p == player}.map{|p| p.display_name }.join(", "), :blue
         player.hear_line player.room.exits_string, :yellow
       end
     end
@@ -68,7 +68,6 @@ module Mud
       end
     end
     CommandList[:admin] << Save.new
-
 
     class Quit < Command
       def initialize
@@ -90,5 +89,70 @@ module Mud
       end
     end
     CommandList[:admin] << Shutdown.new
+
+    class Dig < Command
+      def initialize
+        super("dig", [])
+      end
+      def enact player, args
+        args = args.split(" ",-2) 
+        begin
+          raise "You must enter the name of the new room!" unless args[0]
+          W.add_room(Room.new(args[0].to_sym, "Title", "A generic area."))
+        rescue RuntimeError => e
+          player.hear_line e.to_s, :red
+        end
+      end
+    end
+    CommandList[:builder] << Dig.new
+
+    class Shout < Command
+      def initialize
+        super("shout", [])
+      end
+      def enact player, args
+        W.each_room do |r|
+          r.echo "#{player.display_name} shouts: \"#{args}\"", [player], :red
+        end
+        player.hear_line "You shout: \"#{args}\"", :red
+      end
+    end
+    CommandList[:global] << Shout.new
+
+    class Who < Command
+      def initialize
+        super("who", [])
+      end
+      def enact player, args
+        player.hear_line "Players online: "
+        W.players.each do |p|
+          player.hear_line "\t#{p.name}"
+        end
+      end
+    end
+    CommandList[:global] << Who.new
+
+    class Goto < Command
+      def initialize
+        super("goto", [])
+      end
+      def enact player, args
+        r_name = args.downcase.to_sym
+        if !W.rooms[r_name]
+          player.hear_line "There is no room by that name."
+        elsif W.rooms[r_name] == player.room
+          player.hear_line "You are alredy in that room..."
+        else
+          player.room.echo "#{player.display_name} suddenly dissapears.", [player]
+          player.hear_line "You vanish suddenly, and appear somewhere new..."
+          player.room.remove_player player
+          player.room = W.rooms[r_name]
+          player.room.add_player player
+          player.room.echo "#{player.display_name} suddenly materializes.", [player]
+          player.command "look";
+        end
+      end
+    end
+    CommandList[:admin] << Goto.new
   end
 end
