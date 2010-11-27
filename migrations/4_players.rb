0a33f34e767651c7bfd70f47f536d9a192322d0c
Danny Spencer <dspencer@oberlin.edu>
2010-11-27 18:31:12 -0500

    Added basic stats. Gave player an inventory. fixed up output methods - now you can color different parts of a line, if you want.

--- migrations/4_players.rb
+++ migrations/4_players.rb
@@ -1,6 +1,8 @@
 module Mud
   class Player
-    attr_accessor :connection, :hashed_password, :command_groups
+    attr_accessor :hashed_password, :command_groups
+    attr_accessor :hp, :mp, :max_hp, :max_mp
+    include HasInventory
 
     #accessor
     def room
@@ -19,6 +21,13 @@ module Mud
       @name.capitalize.freeze
     end
 
+    def connection
+      W.player_connection_map[self]
+    end
+    def connection= c
+      W.player_connection_map[self] = c
+    end
+
     # create a new player. This should ONLY be called when a player is CREATED, not logged in.
     def initialize name,hashed_password
       @name = name.downcase
@@ -30,6 +39,17 @@ module Mud
       W.master_players[sym] = self
       self.room = W.default_room
       @command_groups = []
+
+      self.max_hp = 100
+      self.max_mp = 100
+      self.hp= max_hp
+      self.mp= max_mp
+    end
+
+    # when you load the player back into the game from a serialized state, here are some things we
+    # need to fix.
+    def on_load
+      clear_output
     end
 
     # When a player logs into the mud, show some default messages, and add them to the room they
@@ -58,14 +78,31 @@ module Mud
       :blue => "\033[34m"
     }
 
-    # Hear is the method for producing output.
-    def hear_line data, color_attr = []
-      if data.strip != ""
-        color_attr = [color_attr] if color_attr.is_a? Symbol
-        @pending_output += color_attr.map{|a| COLORMAP[a]}.join('')+data+COLORMAP[:off] + "\n"
+    def hear_line *args
+      raise "must give arguments" if args.size == 0
+      args << nil if (args.size%2) ==  1
+      args[args.size-2] += "\n"
+      hear *args
+    end
+
+    def hear *args
+      raise "must give arguments" if args.size == 0
+      args << nil if (args.size%2) ==  1
+      (args.size/2).times do |off|
+        add_output(args[2*off], args[2*off+1])
       end
     end
 
+    # Hear is the method for producing output.
+    def add_output data, color_attr = nil
+      color_attr ||= []
+      if data.strip != ""
+        color_attr = [color_attr] if color_attr.is_a? Symbol
+        @pending_output += color_attr.map{|a| COLORMAP[a]}.join('')+data+COLORMAP[:off]
+      end
+    end
+
+    private :add_output
     # When a player gets input, it might have a shortcut for say.
     # this method will replace a leading ' or " with 'say '
     def preprocess_data data
@@ -84,10 +121,11 @@ module Mud
     # Run a command. Takes an unparsed string. The command knows if its being run directly from input or
     # as the result of something else (like, moving invokes player.command "look")
     def command data, from_input=false
+      data.strip!
+      return unless data != ""
       command_name,args = data.split(' ', 2)
       # if args is nil, lets make it a string.
       args ||= ""
-
       
       if com = Commands::find_command(command_name, @command_groups) 
         com.enact(self, args)
@@ -102,15 +140,34 @@ module Mud
       end
     end
 
-    #generates the proper prompt string
-    def prompt
-      "> "
+    #generates a prompt string.
+    #adds it via hearline
+    def add_prompt
+      hp_percent = 100*hp/max_hp
+      mp_percent = 100*mp/max_mp
+      hp_color = case
+                 when hp_percent > 66
+                   :green
+                 when hp_percent > 33
+                   :yellow
+                 else
+                   :red
+                 end
+      mp_color = case
+                 when mp_percent > 66
+                   :green
+                 when mp_percent > 33
+                   :yellow
+                 else
+                   :red
+                 end
+      hear "#{hp}/#{max_hp} ", [hp_color], "#{mp}/#{max_mp}", [mp_color], " -", nil
     end
 
     #this allows simultanious output to be batched together in the same package.
     def flush_output
       if @pending_output != ""
-        @pending_output += prompt
+        add_prompt
         connection.send_data @pending_output
         clear_output
       end
