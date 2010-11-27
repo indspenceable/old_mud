a68744691adb97e3e13e959d1590f6e132a5a18e
Danny Spencer <dspencer@oberlin.edu>
2010-11-27 18:36:01 -0500

    Impelmented items.

--- migrations/8_items.rb
+++ migrations/8_items.rb
@@ -1,4 +1,30 @@
 module Mud
   class Item
+    def initialize owner
+      @@total_items ||= 0
+      @@blank_ids ||= []
+      @id = @@blank_ids.pop
+      @id ||= @total_items =+ 1 
+      W.items[@id] = self
+      @owner = owner
+      owner.add_item @id
+    end
+    def kill
+      W.items[@id] = nil
+      @id = nil
+      owner.remove_item @id
+    end
+    def move_to new_owner
+      raise "Item.move_to() to an object of type #{new_owner.class} that doesn't have an inventory." unless new_owner.is_a? HasInventory      
+      owner.remove_item @id
+      new_owner.add_item @id
+      self.owner = new_owner
+    end
+  end
+
+  class Sword < Item
+    def display_name
+      "A shining sword"
+    end
   end
 end
