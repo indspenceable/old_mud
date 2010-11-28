2248cb34e1860eef1ae227ac2312208e00dfa849
Danny Spencer <dspencer@oberlin.edu>
2010-11-28 10:59:02 -0500

    Fixed a bug with item owners. Added a intermediate subclass of Item.

--- migrations/8_items.rb
+++ migrations/8_items.rb
@@ -7,24 +7,43 @@ module Mud
       @id ||= @total_items =+ 1 
       W.items[@id] = self
       @owner = owner
-      owner.add_item @id
+      @owner.add_item @id
     end
     def kill
       W.items[@id] = nil
       @id = nil
-      owner.remove_item @id
+      @owner.remove_item @id
     end
     def move_to new_owner
       raise "Item.move_to() to an object of type #{new_owner.class} that doesn't have an inventory." unless new_owner.is_a? HasInventory      
-      owner.remove_item @id
+      @owner.remove_item @id
       new_owner.add_item @id
-      self.owner = new_owner
+      @owner = new_owner
+    end
+    
+    def id? id
+      id == @id
     end
   end
 
-  class Sword < Item
-    def display_name
-      "A shining sword"
+
+  class StandardItem < Item
+    def initialize owner, display_string, name_list
+      super(owner)
+      @display_string = display_string
+      @name_list = name_list
+    end
+    def display_string
+      @display_string
+    end
+    def named? n
+      @name_list.include? n
+    end
+  end
+
+  class Sword < StandardItem
+    def initialize owner
+      super(owner, "A faintly glowing sword", ["sword", "blade"])
     end
   end
 end
