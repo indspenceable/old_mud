91dd723441925754e725f096c704aa8d68195b57
Danny Spencer <dspencer@oberlin.edu>
2010-11-29 13:35:22 -0500

    fixed bug in items. moved HasInventory to the correct location.

--- migrations/8_items.rb
+++ migrations/8_items.rb
@@ -1,10 +1,56 @@
 module Mud
+  #for something that has an inventory
+  module HasInventory
+    #retrieving the items from this character
+    def find_item i
+      items.find{|ci| ci.id?(i) || ci.named?(i)}
+    end
+    def items
+      (@inventory ||= []).map{|id| W.items[id]}.freeze
+    end
+    #moving items around
+    #generally, you don't even want to do do this. Let the item
+    #take care of it for you.
+    def remove_item id
+      remove_from_all_slots id
+      (@inventory ||= []).delete id
+    end
+    def add_item id
+      @inventory << id unless (@inventory ||= []).include? id
+    end
+    #different item slots.
+    def set_item slot, item
+      (@item_slots ||= {})[slot] = (item ? item.id : nil)
+    end
+    def item_for slot
+      if (id = (@item_slots ||= {})[slot])
+        W.items[id]
+      end
+    end
+    def remove_from_all_slots id
+      (@item_slots ||= {}).each_pair do |k,v|
+        @item_slots[k] = nil if v == id
+      end
+    end
+  end
+  W = World.instance
+
+  class Player
+    include HasInventory
+  end
+  class Room
+    include HasInventory
+  end
+
   class Item
     def initialize owner
       @@total_items ||= 0
       @@blank_ids ||= []
       @id = @@blank_ids.pop
-      @id ||= @total_items =+ 1 
+      @id ||= (@@total_items += 1)
+
+      puts "GENERATED AN ID #{@id}"
+
       W.items[@id] = self
       @owner = owner
       @owner.add_item @id
