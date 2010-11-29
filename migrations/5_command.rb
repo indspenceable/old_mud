305bd4ebd4e791d81c552aaed73723d528c753ae
Danny Spencer <dspencer@oberlin.edu>
2010-11-29 00:09:08 -0500

    Command argument processing utitilies added.

--- migrations/5_command.rb
+++ migrations/5_command.rb
@@ -1,5 +1,4 @@
-module Mud
-  module Commands
+module Mud module Commands
     #If a command doesn't override enact, it throws "command not implemented"
     class CommandNotImplementedError < RuntimeError; end
 
@@ -18,13 +17,53 @@ module Mud
       def enact player, args
         raise CommandNotImplementedError.new(self.class.to_s)
       end
-
       ## this is where other helper methods might go, for parsing arguments and such
-
-      #this Raises
+      #this Raises a NoBalanceError (which is caught by player.command)
+      # if the player is offbalance
       def require_balance player, type = :balance
         raise Errors::NoBalanceError.new unless player.on_balance? type
       end
+
+      def process actor, args, types
+        l = args.split(" ", types.length+1)
+        rtn = []
+        types.length.times do |i|
+          if types[i].is_a? Symbol
+            rtn << self.send(types[i], actor, l[i])
+          else
+            rtn << types[i].find { |j| self.send(j, actor, l[i]) }
+          end
+        end
+        rtn
+      end
+
+      def room actor, name
+        W.rooms[name.to_sym]
+      end
+      def exit actor, name
+        actor.room.has_exit? name
+      end
+
+      def item actor, name
+        actor.find_item(name) || actor.room.find_item(name)
+      end
+      def item_from_inventory actor, name
+        actor.find_item name
+      end
+      def item_from_room actor, name
+        actor.room.find_item name
+      end
+
+      def player_online actor, name
+        W.players.include?(W.find_player(name))
+      end
+      def player_here actor, name
+        actor.room.players.find { |p| p.is_named? name }
+      end
+      def player actor, name # => player in the game
+        W.find_player(name) 
+      end
+
     end
 
     # this is a hash of command lists.
