1c789cb1ee7fa75f2962b49996d7b7aaa538f1b5
Danny Spencer <dspencer@oberlin.edu>
2010-12-01 16:08:32 -0500

    finished spec.

--- spec/has_mobiles_spec.rb
+++ spec/has_mobiles_spec.rb
@@ -1,28 +1,29 @@
 require 'migrator'
 
 module Mud
-  describe HasMobiles do
-    describe "the mobiles in this room" do
-      before(:all) do
-        (@o = Object.new).extend(HasMobiles)
-        @mock_mobile = Mobile.new
-      end
-      it "should start with no mobs" do
-        @o.mobiles.size.should be 0
-      end
-      it "should be able to add a mobile" do
-        @o.add_mobile(@mock_mobile)
-        @o.mobiles.size.should be 1
-        #@r.mobiles.include?(@mock_mobile).should == true 
-      end
-      it "should be able to remove a mobile" do
-        #@r.remove_mobile(@mock_mobile)
-        @o.mobiles.size.should be 1
-        #@r.mobiles.include?(@mock_mobile).should == true 
-      end
-      it "should be able to announce when a mob leaves" do
-        #@r.add_mobile(@mock_mobile)
-        #@r.leave_to(@mock_mobile, 'north').arrive_from(@mock_mobile, 'south')
+  module Entities
+    describe HasMobiles do
+      describe "the mobiles in this room" do
+        before(:all) do
+          (@o = Object.new).extend(HasMobiles)
+          @mock_mobile = Mobile.new
+        end
+        it "should start with no mobs" do
+          @o.mobiles.size.should be 0
+        end
+        it "should be able to add a mobile" do
+          @o.add_mobile(@mock_mobile)
+          @o.find_mobile(@mock_mobile).should be @mock_mobile
+          @o.find_mobile(Mobile.new).should be nil
+          @o.mobiles.size.should be 1
+          @o.mobiles.include?(@mock_mobile).should == true 
+        end
+        it "should be able to remove a mobile" do
+          @o.remove_mobile(@mock_mobile)
+          @o.mobiles.size.should be 0
+          @o.mobiles.include?(@mock_mobile).should == false
+          @o.find_mobile(@mock_mobile).should be nil
+        end
       end
     end
   end
